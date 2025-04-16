66c2ea82c8eb36fdba79452aedc938dde4db2349
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-16 11:00:20 +0600

    finding max number

---
+++ E_Max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int x;
+    scanf("%d", &n);
+    int max = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &x);
+        if (x > max)
+        {
+            max = x;
+        }
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
